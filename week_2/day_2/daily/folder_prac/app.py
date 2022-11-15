08a30b17b5181c14b914c4b5dc95c10350a8f7ae
fedau <fedau.d@hotmail.com>
2022-11-15 10:15:13 +0000

    folder setup practice

---
+++ week_2/day_2/daily/folder_prac/app.py
@@ -0,0 +1,13 @@
+# different ways of importing
+
+# from modules.cat import *
+
+# Cat
+
+# from modules.cat import Cat as MyCat
+
+
+from modules import cat
+
+my_cat = cat.Cat()
+
---
+++ week_2/day_2/daily/folder_prac/run_tests.py
@@ -0,0 +1,4 @@
+from tests.test_cat import *
+
+if __name__ == "__main__":
+    unittest.main()
