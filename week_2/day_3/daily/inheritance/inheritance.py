9e4e9a8abfe313e8b6495252a19eb5c7b6f7448d
fedau <fedau.d@hotmail.com>
2022-11-16 10:11:59 +0000

    day 3 start inheritance example

---
+++ week_2/day_3/daily/inheritance/inheritance.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, occupation):
+        self.name = name
+        self.occupation = occupation
+
+    def return_occupation(self):
+        return self.occupation
+
+
+
+class Teacher(Person):
+    def __init__(self, name, occupation, subject):
+        super().__init__(self, name, occupation)
+        self.subject = subject
+
+
