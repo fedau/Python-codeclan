f3a5a653e4846c331d657eb875325595080c8dc6
fedau <fedau.d@hotmail.com>
2022-11-16 10:11:26 +0000

    day 2 complete

--- week_2/day_2/daily/inheritance/inheritance_start_point/classes/vehicle.py
+++ week_2/day_2/daily/inheritance/inheritance_start_point/classes/vehicle.py
@@ -1,7 +1,8 @@
 class Vehicle:
-
-    def __init__(self, price):
-        self.price = price
+# inheritance can be bad because now you have to add a price to all its children
+# favor composition over inheritance
+    # def __init__(self, price):
+    #     self.price = price
 
     def start_engine(self):
         return "Vrrmmm"
