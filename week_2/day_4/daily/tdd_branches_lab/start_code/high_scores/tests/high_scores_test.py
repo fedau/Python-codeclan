0dc1c64487244f880d944c3521da8df8911909b7
fedau <fedau.d@hotmail.com>
2022-11-17 14:56:54 +0000

    first test finished

---
+++ week_2/day_4/daily/tdd_branches_lab/start_code/high_scores/tests/high_scores_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from src.high_scores import latest, personal_best, personal_top_three
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
+
+
+class HighScoresTest(unittest.TestCase):
+    
+    # Tests
+
+    # Test latest score (the last thing in the list)
+    def test_latets_score(self):
+        scores = [12909, 7324, 903, 9023, 38, 209]
+        expected_output = 209
+        actual_output = latest(scores)
+        self.assertEqual(expected_output,actual_output )
+    # Test personal best (the highest score in the list)
+
+    # Test top three from list of scores
+
+    # Test ordered from highest tp lowest
+
+    # Test top three when there is a tie
+
+    # Test top three when there are less than three
+
+    # Test top three when there is only one
+    
