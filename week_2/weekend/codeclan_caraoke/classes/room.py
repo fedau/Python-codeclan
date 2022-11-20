ec00b6f1e93f1d45a74f69e56a7cf65be91b1018
fedau <fedau.d@hotmail.com>
2022-11-20 19:12:39 +0000

    homeworkd extensions

--- week_2/weekend/codeclan_caraoke/classes/room.py
+++ week_2/weekend/codeclan_caraoke/classes/room.py
@@ -1,26 +1,40 @@
+from classes.guest import Guest
+
+
 class Room:
-    def __init__(self, name, capacity):
+    def __init__(self, name, capacity, guests):
         self.name = name
         self.capacity = capacity
-        self.total_in_room = 0
         self.songs = []
+        self.revenue = 0
+        self.people_in_room = []
+
+        for guest in guests:
+            self.add_guest(guest)
 
 
+    def amount_in_room(self):
+            return len(self.people_in_room)
 
-    def add_one_to_room(self):
-        total_in_room = 0
-        total_in_room += 1
-        return total_in_room 
+    def add_guest(self, guest):
+        if self.amount_in_room() < self.capacity:
+            self.people_in_room.append(guest)
+            self.revenue = self.revenue + 2
+            return self.people_in_room 
+        return "room is full"
 
-    def add_guest(guest):
-        people_in_room = []
-        people_in_room.append(guest.name)
-        return people_in_room
-       
+    def get_money_from_guest(self, guest):
+        guest.cash - 2
+        return 2
 
-    def add_song_to_room(song):
-        songs = []
-        songs.append(song.name)
-        return songs
 
+    def add_song_to_room(self, song):
+        self.songs.append(song.name)
+        return self.songs
+
+
+# already_in_room = lamba 2, len(self.people_in_room) : 2 * len(self.people_in_room)
+# revenue = already_in_room 
+# if add_guest():
+# revenue += 2 
 
--- week_2/weekend/codeclan_caraoke/tests/room_test.py
+++ week_2/weekend/codeclan_caraoke/tests/room_test.py
@@ -9,29 +9,44 @@ class TestRoom(unittest.TestCase):
         # self.guest = Drink("beer", 2.00)
         # self.drink_2 = Drink("wine", 3.00)
         # self.drink_3 = Drink("gin", 4.00)
-        self.room = Room("pop", 7)
+     
         self.guest = Guest("Frodo", 10.00)
+        self.guest2 = Guest("William", 17.00)
+        self.guest4 = Guest("William", 17.00)
+
+        self.guest3 = Guest("David", 20.00)
         self.song = Song("we will rock")
-        
+        self.room1 = Room("pop", 7, [self.guest2, self.guest3])
+        self.room2 = Room("Rock", 2,[self.guest4, self.guest3])
+ 
     
     def test_room_has_name(self):
-        self.assertEqual("pop", self.room.name)
+        self.assertEqual("pop", self.room1.name)
 
     def test_room_capacity(self):
-        self.assertEqual(7, self.room.capacity)
-    
+        self.assertEqual(7, self.room1.capacity)
+
+    def test_people_in_room(self):
+        actual_output = self.room1.amount_in_room()
+        self.assertEqual(2, actual_output) 
+
     def test_add_guest_to_room(self):
-        actual_output = Room.add_guest(self.guest)
-        self.assertEqual(["Frodo"], actual_output)
-        # self.assertEqual(1, actual_output)\
+        actual_output = self.room1.add_guest(self.guest)
+        print(self.room1.revenue)
+        self.assertEqual([self.guest2, self.guest3,self.guest], actual_output)
 
-    def test_add_one_to_room(self):
-        actual_output = Room.add_one_to_room(self)
-        self.assertEqual(1, actual_output)
 
+    def test_add_room_full(self):
+        actual_output = self.room2.add_guest(self.guest)
+        self.assertEqual("room is full", actual_output)
 
     def test_add_song(self):
-        actual_output = Room.add_song_to_room(self.song)
+        actual_output = self.room1.add_song_to_room(self.song)
         self.assertEqual(["we will rock"], actual_output)
 
+    def test_revenue(self):
+        pass
+
+    def test_frodo_same(self):
+        self.assertEqual(self.room1.people_in_room[0].name, self.room2.people_in_room[0].name)
 
