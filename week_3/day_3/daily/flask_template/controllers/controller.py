3ba5c5dab030aa1c1282443f9fe9d83a5235d3a8
fedau <fedau.d@hotmail.com>
2022-11-23 10:44:39 +0000

    setup todo list

--- week_3/day_3/daily/flask_template/controllers/controller.py
+++ week_3/day_3/daily/flask_template/controllers/controller.py
@@ -1,6 +1,10 @@
 from app import app
-
+from flask import render_template
 @app.route('/')
 def index():
     return "Goodmorning gurl, live it up today!!"
     
+
+@app.route('/tasks')
+def show_tasks():
+    return render_template('index.html', title='Home')
---
+++ week_3/day_3/daily/flask_template/models/todo_list.py
@@ -0,0 +1,8 @@
+from models.task import Task
+
+
+task1= Task("Cooking dinner", "Making chili")
+task2 = Task("Wash clothes", "Just do the white things")
+
+
+tasks = [task1, task2]
